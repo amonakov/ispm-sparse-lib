0ecb296cf1cd69aaa99b7164106bc1b9b0cb00c6
Alexander Monakov <amonakov@ispras.ru>
2014-08-05 20:15:53 +0400

    util/cuda/allocator.h: STL allocators for pinned and device memory

---
+++ util/cuda/allocator.h
@@ -0,0 +1,77 @@
+#ifndef ISPM_ALLOCATOR_H
+#define ISPM_ALLOCATOR_H
+
+template<typename T>
+class cuda_device_allocator: public std::allocator<T>
+{
+  typedef std::allocator<T> parent;
+public:
+  using typename parent::pointer;
+  using typename parent::size_type;
+  pointer allocate(size_type n, const void * = 0)
+  {
+    pointer p;
+    CUDA_CHECK(cudaMalloc(&p, n * sizeof(T)));
+    CUDA_CHECK(cudaMemset(p, 0, n * sizeof(T)));
+    return p;
+  }
+  void deallocate(pointer p, size_type)
+  {
+    CUDA_CHECK(cudaFree(p));
+  }
+  template<typename O>
+  struct rebind
+  {
+    typedef cuda_device_allocator<O> other;
+  };
+};
+template<typename T1, typename T2>
+inline bool operator==(const cuda_device_allocator<T1> &,
+                       const cuda_device_allocator<T2> &)
+{
+  return true;
+}
+template<typename T1, typename T2>
+inline bool operator!=(const cuda_device_allocator<T1> &,
+                       const cuda_device_allocator<T2> &)
+{
+  return false;
+}
+
+template<typename T>
+class cuda_pinned_allocator: public std::allocator<T>
+{
+  typedef std::allocator<T> parent;
+public:
+  using typename parent::pointer;
+  using typename parent::size_type;
+  pointer allocate(size_type n, const void * = 0)
+  {
+    pointer p;
+    CUDA_CHECK(cudaHostAlloc(&p, n * sizeof(T), cudaHostAllocMapped));
+    return p;
+  }
+  void deallocate(pointer p, size_type)
+  {
+    CUDA_CHECK(cudaFreeHost(p));
+  }
+  template<typename O>
+  struct rebind
+  {
+    typedef cuda_pinned_allocator<O> other;
+  };
+};
+template<typename T1, typename T2>
+inline bool operator==(const cuda_pinned_allocator<T1> &,
+                       const cuda_pinned_allocator<T2> &)
+{
+  return true;
+}
+template<typename T1, typename T2>
+inline bool operator!=(const cuda_pinned_allocator<T1> &,
+                       const cuda_pinned_allocator<T2> &)
+{
+  return false;
+}
+
+#endif
