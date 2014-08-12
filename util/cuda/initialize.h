f0ef4d9ca1fae5706a55aadc9021a26b1df45a11
Alexander Monakov <amonakov@ispras.ru>
2014-08-12 18:25:50 +0400

    util/cuda/initialize: add simple GPU PCI ID getter

--- util/cuda/initialize.h
+++ util/cuda/initialize.h
@@ -11,4 +11,26 @@ ispm_initialize()
   CUDA_CHECK(cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte));
 }
 
+struct DeviceIDString {
+  char str[32];
+};
+
+inline DeviceIDString
+get_device_string()
+{
+  int device;
+  CUDA_CHECK(cudaGetDevice(&device));
+
+  char busid[13];
+  CUDA_CHECK(cudaDeviceGetPCIBusId(busid, sizeof(busid), device));
+
+  struct cudaDeviceProp prop;
+  CUDA_CHECK(cudaGetDeviceProperties(&prop, device));
+
+  DeviceIDString s;
+  snprintf(s.str, sizeof(s.str), "%s (%s)", busid, prop.name);
+
+  return s;
+}
+
 #endif
